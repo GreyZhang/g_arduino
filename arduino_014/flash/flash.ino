bacb8b234a752782e448876cf4c66a86f5e9fca6
GreyZhang <GreyZhang@126.com>
2019-04-24 20:36:36 +0800

    Add a new practice : SD card. Actually my Arduino does not support a SD card.

---
+++ arduino_014/flash/flash.ino
@@ -0,0 +1,19 @@
+/*
+    This project can not be compiled successfully.
+    My arduino doed not support a SD card.
+*/
+
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
+void setup()
+{
+    // put your setup code here, to run once:
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+}
